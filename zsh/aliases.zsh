f3b61ef0b8c14fb36d9152e42a00b06e79a3a643
Ming Teo <ming.teo@foxsports.com.au>
2019-10-29 17:18:18 +1100

    work - addrepo & niw aliases, stop chromium from always downloading

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -72,8 +72,25 @@ alias portk='echo "kill PID, kill -2/-1/-9 PID\n" && kill'
 alias nrb='echo "npm run build\n" && npm run build'
 alias nrbw='echo "npm run build && npm run watch\n" && npm run build && echo "\n\n\n\n\n\n\n\n\n\n----- RUNNING WATCH NOW ------\n\n\n\n\n\n\n\n\n\n" && npm run watch'
 alias nrw='echo "npm run watch\n" && npm run watch'
+alias niw='echo "npm ci && npm run watch\n" && npm ci && npm run watch'
 
-alias addmui='function addmui() { echo "npm i --save \"@kayo/mui@git+ssh://git@bitbucket.foxsports.com.au:7999/mar/mui.git#COMMIT_HASH\"\n" && npm i --save "@kayo/mui@git+ssh://git@bitbucket.foxsports.com.au:7999/mar/mui.git#$1"; }; addmui'
+alias addmui='echo "deprecated, use addrepo\n" && addrepo'
+alias addrepo='function addrepo() {
+    echo "Commit hash: ";
+    read COMMIT_HASH;
+    COMMIT=${COMMIT_HASH:-poopooeverywhereitneverstops}
+    REPO=${1:-mui};
+    PROJECT=${2:-mar};
+    PACKAGE="@kayo/$REPO@git+ssh://git@bitbucket.foxsports.com.au:7999/$PROJECT/$REPO.git#$COMMIT";
+
+    if [ "$COMMIT" = "poopooeverywhereitneverstops" ]
+    then
+        echo "Please enter a valid commit hash";
+    else
+        echo "npm i --save \"$PACKAGE\"\n";
+        npm i --save \"$PACKAGE\";
+    fi;
+}; addrepo'
 
 alias viz='function viz() {
     echo "npm run viz:golden OR npm run viz:specified TEST_NAME\n";
@@ -84,6 +101,40 @@ alias viz='function viz() {
         npm run viz:specified $1
     fi;
 }; viz'
+
+alias et='function e2etest() {
+    if [ ! $1 ]
+    then
+        echo "Package version [$1]:";
+        read READ_PACKAGE_VER;
+    fi;
+    PACKAGE_VER=${READ_PACKAGE_VER:-$1};
+
+    if [ ! $2 ]
+    then
+        echo "QA build number [$2]:"
+        read READ_QA_BUILD_NUM;
+    fi;
+    QA_BUILD_NUM=${READ_QA_BUILD_NUM:-$2};
+
+    if [ ! $3 ]
+    then
+        echo "Local [y]:"
+        read READ_IS_LOCAL;
+    fi;
+    IS_LOCAL=${READ_IS_LOCAL:-y};
+    TEST_TYPE="local";
+
+    if [ "$IS_LOCAL" != "y" ]
+    then
+        TEST_TYPE="docker";
+    fi;
+
+    CMD="FISO_ENV=staging MARTIAN_CTV_WIDGETS_VERSION=$PACKAGE_VER-qa$QA_BUILD_NUM npm run e2e:$TEST_TYPE";
+
+    echo "$CMD\n";
+    eval $CMD;
+}; e2etest'
 
 alias getperms='echo "stat -f \"%OLp\"\n" && stat -f "%OLp"'
 
